21981931b86587b8fdc9256f05923ec7348a032e
kibmatar <d.trigorlov@mail.ru>
2021-01-21 17:23:06 +0300

    new commit here

---
+++ Task2/Counter.java
@@ -0,0 +1,18 @@
+package eu.Senla.Task2;
+
+public class Counter {
+	int firstField;
+	private double secondField;
+	
+	public Counter() {
+		
+	}
+	
+	public int getFirstField() {
+		return firstField;
+	}
+	public void setFirstField(int firstField) {
+		this.firstField=firstField;
+	}
+	
+}
